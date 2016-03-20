8c9f6e9e1967f9dabd8d03f87e760720ea5fa76e
Adrian Hall <adrian@shellmonger.com>
2016-03-19 21:36:11 -0700

    Reverting changes from day-5

--- tables/TodoItem.js
+++ tables/TodoItem.js
@@ -3,9 +3,4 @@ var azureMobileApps = require('azure-mobile-apps');
 // Create a new table definition
 var table = azureMobileApps.table();
 
-table.read(function (context) {
-    console.log('user = ', context.user);
-    return context.execute();
-});
-
 module.exports = table;
