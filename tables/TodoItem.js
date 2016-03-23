14571f0fe31aec0b7ca3149004485db0c7351909
Adrian Hall <adrian@shellmonger.com>
2016-03-23 16:39:08 -0700

    Added authentication to the table controller

--- tables/TodoItem.js
+++ tables/TodoItem.js
@@ -3,4 +3,7 @@ var azureMobileApps = require('azure-mobile-apps');
 // Create a new table definition
 var table = azureMobileApps.table();
 
+// Require authentication
+table.access = 'authenticated';
+
 module.exports = table;
