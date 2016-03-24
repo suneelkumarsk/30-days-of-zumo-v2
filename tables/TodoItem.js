8b412484cd482c257df89c7ae5b07fe189b2d7d5
Adrian Hall <adrian@shellmonger.com>
2016-03-23 17:09:28 -0700

    Personal Table implementation

--- tables/TodoItem.js
+++ tables/TodoItem.js
@@ -6,4 +6,29 @@ var table = azureMobileApps.table();
 // Require authentication
 table.access = 'authenticated';
 
+// CREATE operation
+table.insert(function (context) {
+  context.item.userId = context.user.id;
+  return context.execute();
+});
+
+// READ operation
+table.read(function (context) {
+  context.query.where({ userId: context.user.id });
+  return context.execute();
+});
+
+// UPDATE operation
+table.update(function (context) {
+  context.query.where({ userId: context.user.id });
+  context.item.userId = context.user.id;
+  return context.execute();
+});
+
+// DELETE operation
+table.delete(function (context) {
+  context.query.where({ userId: context.user.id });
+  return context.execute();
+});
+
 module.exports = table;
