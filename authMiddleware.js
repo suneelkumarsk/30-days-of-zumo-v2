8ef54d9e807477ace28b07165e0ef6e32b7d9808
Adrian Hall <adrian@shellmonger.com>
2016-03-29 12:55:40 -0700

    Claims fixes / comments

--- authMiddleware.js
+++ authMiddleware.js
@@ -2,20 +2,25 @@ var authCache = {};
 
 function authMiddleware(request, response, next) {
   if (typeof request.azureMobile.user.id !== 'undefined') {
+
+    // Check to see if the user is in the cache
     if (typeof authCache[request.azureMobile.user.id] !== 'undefined') {
       request.azureMobile.user.emailaddress = authCache[request.azureMobile.user.id];
-      next();
+      return next();
     }
+
+    // If not in the cache then call getIdentity() to get the information
     request.azureMobile.user.getIdentity().then(function (userInfo) {
-      console.log('USER INFO CLAIMS: ', JSON.stringify(userInfo.aad.claims, 2);
+      console.log('USER INFO CLAIMS: ', JSON.stringify(userInfo.aad.claims, 2));
       if (typeof userInfo.aad.claims.emailaddress !== 'undefined') {
         authCache[request.azureMobile.user.id] = userInfo.aad.claims.emailaddress;
         request.azureMobile.user.emailaddress = authCache[request.azureMobile.user.id];
       }
-      next();
+      return next();
     });
+
   } else {
-    next();
+    return next();
   }
 }
 
