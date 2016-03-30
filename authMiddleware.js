c3de1393f4e912ac94850f788d72818332b2caa7
Adrian Hall <adrian@shellmonger.com>
2016-03-29 17:47:20 -0700

    Fixie #7

--- authMiddleware.js
+++ authMiddleware.js
@@ -20,26 +20,39 @@ function groupReducer(target, claim) {
  * @returns {any} the result of the next middleware
  */
 function authMiddleware(request, response, next) {
-    if (typeof request.azureMobile.user === 'undefined')
+    if (typeof request.azureMobile.user === 'undefined') {
+        console.info('request.azureMobile.user is not set');
         return next();
-    if (typeof request.azureMobile.user.id === 'undefined')
+    }
+    if (typeof request.azureMobile.user.id === 'undefined') {
+        console.info('request.azureMobile.user.id is not set');
         return next();
+    }
 
     var user = request.azureMobile.user;
     if (typeof authCache[user.id] === 'undefined') {
-        user.getIdentity().then(function (userInfo) {
+        console.info('user', user.id, 'does not exist in authCache');
+        return user.getIdentity().then(function (userInfo) {
+            console.info('back from getIdentity: emailaddress = ', userInfo.claims.aad.emailaddress);
+            var groups = userInfo.aad.user_claims.reduce(groupReducer, []);
+            console.info('groups = ', groups);
             authCache[user.id] = {
                 emailaddress: userInfo.claims.aad.emailaddress,
-                groups: userInfo.aad.user_claims.reduce(groupReducer, [])
+                groups: groups
             };
 
-            user.emailaddress = authCache[user.id].emailaddress;
-            user.groups = authCache[user.id].groups;
+            console.log('storing emailaddress in user object');
+            request.azureMobile.user.emailaddress = authCache[user.id].emailaddress;
+            console.log('storing groups in user object');
+            request.azureMobile.user.groups = authCache[user.id].groups;
+            console.log('calling next');
             return next();
         });
     } else {
+        console.info('user', user.id, 'exists in cache');
         user.emailaddress = authCache[user.id].emailaddress;
         user.groups = authCache[user.id].groups;
+        console.info('calling next');
         return next();
     }
 }
