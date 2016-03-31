121183af80df260c52b2b4d885924e84ca576e76
Adrian Hall <adrian@shellmonger.com>
2016-03-31 14:10:09 -0700

    Rename of TodoItem.js

---
+++ tables/friends.js
@@ -0,0 +1,34 @@
+var azureMobileApps = require('azure-mobile-apps');
+
+// Create a new table definition
+var table = azureMobileApps.table();
+
+// Require authentication
+table.access = 'authenticated';
+
+// CREATE operation
+table.insert(function (context) {
+    context.item.userId = context.user.emailaddress;
+    return context.execute();
+});
+
+// READ operation
+table.read(function (context) {
+    context.query.where({ userId: context.user.emailaddress });
+    return context.execute();
+});
+
+// UPDATE operation
+table.update(function (context) {
+    context.query.where({ userId: context.user.emailaddress });
+    context.item.userId = context.user.emailaddress;
+    return context.execute();
+});
+
+// DELETE operation
+table.delete(function (context) {
+    context.query.where({ userId: context.user.emailaddress });
+    return context.execute();
+});
+
+module.exports = table;
