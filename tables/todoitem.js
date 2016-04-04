7c6dc004d20ed7db10c9ac18ed386f17a7fc007d
Adrian Hall <adrian@shellmonger.com>
2016-04-04 13:02:36 -0700

    change #8

--- tables/todoitem.js
+++ tables/todoitem.js
@@ -22,7 +22,7 @@ table.read(function (context) {
         .then(function (friends) {
             var list = friends.map(function (f) { return f.userId; }).push(context.user.emailaddress);
             console.log('READ: friends = ', list);
-            context.query.where(function(friends) { return this.userId in friends; }, list);
+            context.query.where(function(friends) { return friends.indexOf(this.userId) >= 0; }, list);
             return context.execute();
         });
 //    context.query.where({ userId: context.user.emailaddress });
