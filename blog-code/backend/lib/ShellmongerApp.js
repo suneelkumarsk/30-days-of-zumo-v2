014c7862f973ec29d609171fcff4d3b353ad9453
Adrian Hall <adrian@shellmonger.com>
2016-04-07 13:43:51 -0700

    Backout of change

--- blog-code/backend/lib/ShellmongerApp.js
+++
@@ -1,7 +0,0 @@
-﻿import * as WindowsAzure from 'azure-mobile-apps-client';
-
-export default class ShellmongerApp {
-    constructor() {
-    this.client = new WindowsAzure.MobileServiceClient(this.clientUrl);
-    }
-}
