69c84b62a16e8ddfa72518bd15f4ab8ba15c7315
Adrian Hall <adrian@shellmonger.com>
2016-04-06 22:15:14 -0700

    Added initial version of the library

---
+++ blog-code/backend/lib/ShellmongerApp.js
@@ -0,0 +1,7 @@
+﻿import * as WindowsAzure from 'azure-mobile-apps-client';
+
+export default class ShellmongerApp {
+    constructor() {
+    this.client = new WindowsAzure.MobileServiceClient(this.clientUrl);
+    }
+}
