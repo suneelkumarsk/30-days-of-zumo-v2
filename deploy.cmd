de2d785300303ead54847c7a734da119585685f3
Adrian Hall <adrian@shellmonger.com>
2016-05-05 16:59:21 -0700

    new deployment script

--- deploy.cmd
+++ deploy.cmd
@@ -23,7 +23,7 @@ setlocal enabledelayedexpansion
 SET ARTIFACTS=%~dp0%..\artifacts
 
 IF NOT DEFINED DEPLOYMENT_SOURCE (
-  SET DEPLOYMENT_SOURCE=%~dp0%.
+  SET DEPLOYMENT_SOURCE=%~dp0%\blog-code\backend
 )
 
 IF NOT DEFINED DEPLOYMENT_TARGET (
