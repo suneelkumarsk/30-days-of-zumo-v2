f5c253092e508e81e108a13e18baf98c4bf544b2
Adrian Hall <adrian@shellmonger.com>
2016-05-11 15:22:42 -0700

    Removed Client.XForms

--- blog-code/Client.XForms/Client.XForms/Services/ServiceLocations.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Client.XForms.Services
-{
-    public static class ServiceLocations
-    {
-        public static readonly string AzureServiceUrl = "https://shellmonger-demo.azurewebsites.net";
-    }
-}
